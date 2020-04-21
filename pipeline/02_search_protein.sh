415553f8ca79172031d25f38d3eda005747f4995
Jason Stajich <jasonstajich.phd@gmail.com>
2020-04-20 22:54:54 -0700

    HMMER search of NCVOGs against proteomes

---
+++ pipeline/02_search_protein.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+#SBATCH -p short -N 1 -n 2 --mem 4gb --out logs/pepsearch.%a.log
+
+module load hmmer
+
+CPUS=$SLURM_CPUS_ON_NODE
+if [ -z $CPUS ]; then
+ CPUS=1
+fi
+CPU=$CPUS
+N=${SLURM_ARRAY_TASK_ID}
+
+if [ -z $N ]; then
+    N=$1
+    if [ -z $N ]; then
+        echo "Need an array id or cmdline val for the job"
+        exit
+    fi
+fi
+PEP=proteins
+QUERY=db/NCVOG/NCVOG.hmmb
+OUT=search/protein
+mkdir -p $OUT
+TARGET=$(ls $PEP/*.aa.fasta | sed -n ${N}p)
+OUTFILE=$OUT/$(basename $TARGET .aa.fasta).hmmscan
+
+if [[ ! -f $OUTFILE.done || $TARGET -nt $OUTFILE.done ]]; then
+	hmmscan -E 1e-8 --cpu $CPU --domtblout $OUTFILE.domtbl $QUERY $TARGET > $OUTFILE.log
+  touch $OUTFILE.done
+fi
