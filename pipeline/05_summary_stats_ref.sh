44e725dd742e7b8b3bd6e71b3ba5877aaa8d2248
Jason Stajich <jasonstajich.phd@gmail.com>
2020-05-23 05:13:22 -0700

    tweaked but deleting

--- pipeline/05_summary_stats_ref.sh
+++ pipeline/05_summary_stats_ref.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH -p short -N 1 -n 2 --mem 4gb --out logs/summary_stats_ref.%a.log
+#SBATCH -p short -N 1 -n 2 --mem 4gb --out logs/summary_stats_ref.log
 
 module load prodigal
 module unload miniconda2
@@ -21,49 +21,43 @@ if [ -z $CPUS ]; then
  CPUS=1
 fi
 CPU=$CPUS
-N=${SLURM_ARRAY_TASK_ID}
+for INGENOME in $(ls $GENOMES/*.nt.fasta)
+do
+  BASE=$(basename $INGENOME .nt.fasta)
 
-if [ -z $N ]; then
-    N=$1
-    if [ -z $N ]; then
-        echo "Need an array id or cmdline val for the job"
-        exit
-    fi
-fi
-
-INGENOME=$(ls $GENOMES/*.nt.fasta | sed -n ${N}p)
-BASE=$(basename $INGENOME .nt.fasta)
-
-echo $BASE
-if [ ! -f $INGENOME ]; then
-  echo "cannot find a genome in $GENOMES for *.spades.fasta.gz"
-  exit
-fi
-if [[ ! -f $OUTPRED/$BASE.prodigal.faa || $INGENOME -nt $OUTPRED/$BASE.prodigal.faa ]]; then
-	prodigal -a $OUTPRED/$BASE.prodigal.faa -d $OUTPRED/$BASE.prodigal.cds -f gff -i $INGENOME -p meta -o $OUTPRED/$BASE.prodigal.gff
-fi
-
-if [[ ! -s $SEARCHRRNA/$BASE.rRNA_search.tab.gz ]]; then
-  module load ncbi-blast/2.9.0+
-  blastn -db $FUNGIRRNADB -num_threads $CPU -query $INGENOME -evalue 1e-3  -outfmt 6 -max_target_seqs 5 -out $SEARCHRRNA/$BASE.rRNA_search.tab
-  pigz -k $SEARCHRRNA/$BASE.rRNA_search.tab
-  module unload ncbi-blast
-fi
-
-if [[ ! -f $SEARCHMT/$BASE.MT_search.tab ]]; then
-  module load fasta
-  tfasty36 -E 1e-5 -m 8c -T $CPU $FUNGIMTPEP $INGENOME > $SEARCHMT/$BASE.MT_search.tab
-  if [ ! -s $SEARCHMT/$BASE.MT_search.tab ]; then
-    pigz -k $SEARCHMT/$BASE.MT_search.tab
+  echo $BASE
+  if [[ ! -f $OUTPRED/$BASE.prodigal.faa || $INGENOME -nt $OUTPRED/$BASE.prodigal.faa ]]; then
+    echo "Expected to have already run prodigal and pre-screen Pfam? for $BASE ($OUTPRED/$BASE.prodigal.faa) or genome is newer than prodigal file"
+    continue
+	  # prodigal -a $OUTPRED/$BASE.prodigal.faa -d $OUTPRED/$BASE.prodigal.cds -f gff -i $INGENOME -p meta -o $OUTPRED/$BASE.prodigal.gff
   fi
-  module unload fasta
-fi
 
-EXTRA=""
-if [ -s $SEARCHDLV/$BASE.nt.TFASTX.tab ]; then
-  EXTRA="$EXTRA -sd $SEARCHDLV/$BASE.nt.TFASTX.tab"
-fi
-if [ -s $SEARCHVOG/$BASE.nt.TFASTX.tab  ]; then
-  EXTRA="$EXTRA -sv $SEARCHVOG/$BASE.nt.TFASTX.tab"
-fi
-python3 scripts/genome_stats_for_viral_ML.py --prodigal $OUTPRED/$BASE.prodigal.gff --outbase $OUTREPORT/$BASE --ribosomal $SEARCHRRNA/$BASE.rRNA_search.tab --mitochondria $SEARCHMT/$BASE.MT_search.tab $EXTRA -i $INGENOME
+  if [[ ! -s $SEARCHRRNA/$BASE.rRNA_search.tab.gz ]]; then
+    module load ncbi-blast/2.9.0+
+    blastn -db $FUNGIRRNADB -num_threads $CPU -query $INGENOME -evalue 1e-8 -subject_besthit -outfmt 6 -max_target_seqs 5  -out $SEARCHRRNA/$BASE.rRNA_search.tab
+    pigz -k $SEARCHRRNA/$BASE.rRNA_search.tab
+    module unload ncbi-blast
+  fi
+
+  if [[ ! -f $SEARCHMT/$BASE.MT_search.tab ]]; then
+    module load fasta
+    tfasty36 -E 1e-5 -m 8c -T $CPU $FUNGIMTPEP $INGENOME > $SEARCHMT/$BASE.MT_search.tab
+    if [ ! -s $SEARCHMT/$BASE.MT_search.tab ]; then
+      pigz -k $SEARCHMT/$BASE.MT_search.tab
+    fi
+    module unload fasta
+  fi
+
+  EXTRA=""
+  if [ -s $SEARCHDLV/$BASE.hmmsearch.domtbl ]; then
+    EXTRA="-sd $SEARCHDLV/$BASE.hmmsearch.domtbl"
+  fi
+  if [ -s $SEARCHVOG/$BASE.hmmsearch.domtbl ]; then
+    EXTRA="$EXTRA -sv $SEARCHVOG/$BASE.hmmsearch.domtbl"
+  fi
+  if [ -s $SEARCHPFAM/$BASE.pfamscan.domtbl ]; then
+    EXTRA="$EXTRA --pfam $SEARCHPFAM/$BASE.pfamscan.domtbl"
+  fi
+  python3 scripts/genome_stats_for_viral_ML.py --prodigal $OUTPRED/$BASE.prodigal.gff --outbase $OUTREPORT/$BASE \
+    --ribosomal $SEARCHRRNA/$BASE.rRNA_search.tab --mito $SEARCHMT/$BASE.MT_search.tab $EXTRA -i $INGENOME
+done
